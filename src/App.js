c3989ea4f548688780bae2e55f99fb39c8505243
source <mom.sombrathna26@gmail.com>
2023-01-07 13:31:04 +0700

    add new welcome page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import Home from './Components/Home';
 import ErrorPage from './Components/ErrorPage';
-import Login from './Components/Login';
 import ReadBook from './Components/ReadBook';
 import Save from './Components/Save';
-import SignIn from './Components/SignIn';
 import WelcomePage from './Components/WelcomePage';
 
 const App = () => {
@@ -14,10 +12,9 @@ const App = () => {
         <Routes>
           <Route path='/' element={<WelcomePage />}/>
           <Route path='/home' element={<Home />}/>
-          <Route path='/login' element={<Login />}/>
+
           <Route path='/readbook' element={<ReadBook />}/>
           <Route path='/save' element={<Save />}/>
-          <Route path='/signin' element={<SignIn />}/>
           <Route path='*' element={<ErrorPage />}/>
         </Routes>
     </Router>
