d35901b9049c1722e953befcb68cc6ccf674ebdb
source <mom.sombrathna26@gmail.com>
2023-01-18 11:43:27 +0700

    update footer

--- src/Components/Costumize/Footer/Footer.jsx
+++ src/Components/Costumize/Footer/Footer.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ItemContainer from "./ItemContainer";
+import { Icon } from 'react-icons-kit';
+import {facebook} from 'react-icons-kit/icomoon/facebook';
+import {google} from 'react-icons-kit/icomoon/google';
+import {telegram} from 'react-icons-kit/icomoon/telegram';
+import {instagram} from 'react-icons-kit/icomoon/instagram';
+import {github} from 'react-icons-kit/icomoon/github';
+
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white">
-      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
+    <footer className="bg-zinc-900 text-white">
+      {/* <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
         <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
           <span className="text-blue-600">Cam-Library</span></h1>
         <div>
@@ -12,11 +19,19 @@ const Footer = () => {
             Search
           </button>
         </div>
-      </div>
+      </div> */}
       <ItemContainer/>     
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 text-center pt-2 text-gray-400 text-sm pb-8">
+      <div className="flex lg:flex-row md:flex-col sm-flex-col flex-col lg:gap-36 md:gap-20 sm:gap-10 gap-5 justify-center items-center text-center pt-2 text-gray-400 text-sm pb-8">
         <span>© 2023 Cam-Library. All rights reserved.</span>
         <span>Terms · Privacy Policy</span>
+        <span className="flex flex-row gap-5">
+          <a href=""><Icon icon={facebook} size="1.5rem"/></a>
+          <a href=""><Icon icon={google} size="1.5rem"/></a>
+          <a href=""><Icon icon={instagram} size="1.5rem"/></a>
+          <a href=""><Icon icon={telegram} size="1.5rem"/></a>
+          <a href=""><Icon icon={github} size="1.5rem"/></a>
+
+        </span>
       </div>
       
     </footer>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -16,9 +16,11 @@ const Home = () => {
 
            <div className='w-md pt-16 2xl:px-60 xl:px-0 lg:px-0 md:px-0 sm:px-0'>
            <h1 className='mb-5 2xl:pl-20 xl:pl-32 lg:pl-16 md:pl-10 sm:pl-10 pl-10 font-bold text-2xl'>History books</h1>
-           <BookList /> 
+            <BookList /> 
            </div>
-           <Footer/>
+           <div className=" mt-5">
+            <Footer/>
+           </div>
        </main>
 
         </>
