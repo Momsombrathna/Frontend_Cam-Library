600e8d006aef20d968b56c5407b007e9a108a728
chantpanha <87080443+sniffex@users.noreply.github.com>
2023-02-03 20:45:32 +0700

    update book

--- src/Components/BookCategories/ClassicBook.jsx
+++ src/Components/BookCategories/ClassicBook.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { book7 } from '../Data/BookData';
+import { book } from '../Data/BookData';
 import { AiOutlineDownload } from "react-icons/ai";
 
 const ClassicBook = (props) => {
@@ -9,7 +9,7 @@ const ClassicBook = (props) => {
             <div className='flex items-stretch'>
                 <div id='slider' className=' w-full rounded-lg h-auto p-2 inline-block whitespace-nowrap overflow-x-scroll'>
                     {
-                        book7.map((item)=>{
+                        book.map((item)=>{
                             return(
                                 <div className=' inline-block'>
                                 <div class=" mr-2 max-w-md bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
