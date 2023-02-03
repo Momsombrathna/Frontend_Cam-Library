3e17790154a5ad88b2212aed83326e0dd94cc10f
chantpanha <87080443+sniffex@users.noreply.github.com>
2023-02-03 20:01:34 +0700

    add book

--- src/Views/HomeLogin.js
+++ src/Views/HomeLogin.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
 import Footer from '../Components/Footer';
 import NavLog from '../Components/NavLog';
-
-
+import Classicbook from '../Components/BookCategories/ClassicBook';
+import AdventureStoriesBook from '../Components/BookCategories/AdventureStoriesBook';
+import BestBookOfTheWeek from '../Components/BookCategories/BestBookOfTheWeek';
+import HistoricalFictionBook from '../Components/BookCategories/HistoricalFictionBook'
 const HomeLogin = () => {
 
     return (
@@ -16,13 +17,24 @@ const HomeLogin = () => {
             <main className=" bg-slate-100 w-full h-rotate-0">
                 
             <div className='w-md pt-32 2xl:px-60 xl:px-0 lg:px-0 md:px-0 sm:px-0'>
-            <h1 className='mb-5 2xl:pl-20 xl:pl-32 lg:pl-16 md:pl-10 sm:pl-10 pl-10 font-bold text-2xl'>Classic books</h1>
-            
+            <h1 className='mb-5 2xl:pl-20 xl:pl-32 lg:pl-16 md:pl-10 sm:pl-10 pl-10 font-bold text-2xl'>Best book of the week</h1>
+            <BestBookOfTheWeek/>
             </div>
 
             <div className='w-md pt-16 2xl:px-60 xl:px-0 lg:px-0 md:px-0 sm:px-0'>
-            <h1 className='mb-5 2xl:pl-20 xl:pl-32 lg:pl-16 md:pl-10 sm:pl-10 pl-10 font-bold text-2xl'>History books</h1>
-            
+            <h1 className='mb-5 2xl:pl-20 xl:pl-32 lg:pl-16 md:pl-10 sm:pl-10 pl-10 font-bold text-2xl'>Historical Fiction books</h1>
+            <HistoricalFictionBook/>
+            </div>
+
+
+            <div className='w-md pt-16 2xl:px-60 xl:px-0 lg:px-0 md:px-0 sm:px-0'>
+            <h1 className='mb-5 2xl:pl-20 xl:pl-32 lg:pl-16 md:pl-10 sm:pl-10 pl-10 font-bold text-2xl'>Classic books</h1>
+            <AdventureStoriesBook/>
+            </div>
+
+            <div className='w-md pt-16 2xl:px-60 xl:px-0 lg:px-0 md:px-0 sm:px-0'>
+            <h1 className='mb-5 2xl:pl-20 xl:pl-32 lg:pl-16 md:pl-10 sm:pl-10 pl-10 font-bold text-2xl'>AdventureStories books</h1>
+            <Classicbook/>
             </div>
             <div className=" mt-5">
                 <Footer/>
